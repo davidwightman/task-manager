825ac7bae449f5f76c66252730a43463d31e861d
David Wightman <david@davidwightman.org>
2019-09-29 11:32:11 -0400

    pagination and file uploads

--- src/index.js
+++ src/index.js
@@ -8,11 +8,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) =>{
-//         res.status(503).send('error: site under work')
-// })
+const multer = require('multer')
 
-// auto parse incoming json to object and acces in req handlers
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
+
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -21,15 +29,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-// const main = async () => {
-//     // const task = await Task.findById("5d8f8e608dec92f48fe02239")
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById("5d8f8d5dca9835f437189821")
-//     // await user.populate('owner').execPopulate()
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -19,19 +19,29 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true
+// GET /tasks?limit&skip=0
+// GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
 
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
     try {
         //const tasks = await Task.find({ owner: req.user._id })
         await req.user.populate({
             path: 'tasks',
-            match: {
-                completed: false
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.status(200).send(req.user.tasks)
