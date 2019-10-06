9018519d65e73b7cc84555f336d5b0c7a6e268a2
David Wightman <david@davidwightman.org>
2019-10-06 12:00:44 -0400

    adding tests

---
+++ tests/math.test.js
@@ -0,0 +1,54 @@
+const { add, calculateTip, celsiusToFahrenheit, fahrenheitToCelsius} = require('../src/math')
+
+test('should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+
+    expect(total).toBe(13)
+    // if (total !== 13) {
+    //     throw new Error('Total tip should be 13. got ' + total)
+    // }
+})
+
+test('should calculate total with default tip', () =>{
+    const total = calculateTip(10)
+    expect(total).toBe(12)
+})
+
+test('should convert 32 F to 0 C', () => {
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('should convert 0 C to 32 F', () => {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
+})
+
+
+// test('async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000
+// )
+    
+// })
+
+test('should add two numbers', (done) => {
+    add(3, 2).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('should add two numbers async/await', async () => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
+})
+// test('Hello world!', () => {
+
+// })
+
+// test('This should fail', ()=> {
+//     throw new Error('fail')
+// })
